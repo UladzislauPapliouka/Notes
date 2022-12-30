5cb809b74fadfaab2c813e5878adb06fb9700cf4
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2022-12-31 01:17:14 +0300

    Upgrade MainInput

--- src/Components/MainInput/MainInput.tsx
+++ src/Components/MainInput/MainInput.tsx
@@ -1,10 +1,50 @@
-import React, {FC} from "react"
-import {OutlinedTextFieldProps, TextField} from "@mui/material"
-import  MainInputStyle from "./MainInput.module.scss"
+import React, {ChangeEvent, FC, useState} from "react"
+import {FormControl, IconButton, InputAdornment, OutlinedInput, OutlinedTextFieldProps} from "@mui/material"
+import MainInputStyle from "./MainInput.module.scss"
+import AddIcon from "@mui/icons-material/Add"
+
 type IMainInput = OutlinedTextFieldProps
 
-const MainInput: FC<IMainInput> = ({...props}) =>
-	<TextField className={MainInputStyle.textField} {...props}/>
+const MainInput: FC<IMainInput> = ({...props}) => {
+	const [noteTitle, setNoteTitle] = useState<string>("")
+	const onAddClickHandler = () => {
+		//TODO: Add usage of callback to add new note
+		console.log(noteTitle)
+		setNoteTitle("")
+	}
+	const onChangeInputHandler = (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => setNoteTitle(event.target.value)
+	const onEnterInputHandler = (event: React.KeyboardEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+		if (event.code === "Enter") {
+			onAddClickHandler()
+		}
+	}
+	return (
+		<FormControl>
+			<OutlinedInput
+				type={"text"}
+				placeholder={"Note title..."}
+				sx={{
+					width: "500px"
+				}}
+				value={noteTitle}
+				onChange={(event) => onChangeInputHandler(event)}
+				onKeyDown={(event) => {
+					onEnterInputHandler(event)
+				}}
+				endAdornment={
+					<InputAdornment position="end">
+						<IconButton
+							onClick={onAddClickHandler}
+							edge="end"
+						>
+							<AddIcon/>
+						</IconButton>
+					</InputAdornment>
+				}
+			/>
+		</FormControl>
+	)
+}
 
 export {MainInput}
 
